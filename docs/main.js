c7a77c3f7358cc03f1770e57c6d2bd4319fdc100
Miuko Kawasaki <miuko.kawasaki@gmail.com>
2020-06-14 12:54:57 +0900

    スクロールに合わせてテキストが切り替わるようにしてみる

--- docs/main.js
+++ docs/main.js
@@ -3,7 +3,7 @@ $(function () {
 
   new ScrollMagic.Scene({
     triggerElement: '#wrapper',
-    duration: 2000,
+    duration: 6000,
   })
   .setTween(TweenMax.to('#small-gear', 1, { rotation: 600, ease: Linear.easeNone }))
   .setPin('#small-gear')
@@ -11,21 +11,44 @@ $(function () {
 
   new ScrollMagic.Scene({
     triggerElement: '#wrapper',
-    duration: 2000,
+    duration: 6000,
   })
   .setTween(TweenMax.to('#large-gear', 1, { rotation: -400, ease: Linear.easeNone }))
   .setPin('#large-gear')
   .addTo(controller)
 
-  new ScrollMagic.Scene({
-    triggerElement: '#wrapper',
-  })
-  .setPin('.headline')
-  .addTo(controller)
+  const setActive = (headline, content, offset, duration) => {
+    new ScrollMagic.Scene({
+      triggerElement: '#wrapper',
+    })
+    .setPin(headline)
+    .addTo(controller)
 
-  new ScrollMagic.Scene({
-    triggerElement: '#wrapper',
-  })
-  .setPin('.content')
-  .addTo(controller)
+    new ScrollMagic.Scene({
+      triggerElement: '#wrapper',
+    })
+    .setPin(content)
+    .addTo(controller)
+
+    new ScrollMagic.Scene({
+      triggerElement: headline,
+      offset,
+      duration,
+    })
+    .setClassToggle(headline, 'active')
+    .addTo(controller)
+
+    new ScrollMagic.Scene({
+      triggerElement: content,
+      offset: offset - 171, // 同時に切り替わるよう微調整
+      duration,
+    })
+    .setClassToggle(content, 'active')
+    .addTo(controller)
+  }
+
+ setActive('#myself-headline', '#myself-content', 0, 2000)
+ setActive('#product-1-headline', '#product-1-content', 2000, 1000)
+ setActive('#product-2-headline', '#product-2-content', 3000, 1000)
+ setActive('#last-headline', '#last-content', 4000, 2000)
 })
